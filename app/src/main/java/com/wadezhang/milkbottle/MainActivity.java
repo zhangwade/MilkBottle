9e07ca7fb59729c3f0c9dcad2e71aedb4e11c957
WadeZhang <wade354638526@163.com>
2017-02-07 18:03:22 +0800

    WadeZhang 2017.2.7

--- app/src/main/java/com/wadezhang/milkbottle/MainActivity.java
+++ app/src/main/java/com/wadezhang/milkbottle/MainActivity.java
@@ -6,11 +6,18 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by zhangxix on 2017/1/24.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener {
+
+    @BindView(R.id.bottom_navigation_bar) BottomNavigationBar mBottomNavigationBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         SystemClock.sleep(2000);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
